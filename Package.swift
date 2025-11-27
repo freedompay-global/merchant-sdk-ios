b7cfd3041118a26046d21dfe8eef4238fc2feb4b
Erkhaan Struchkov <erkhaan.struchkov@freedompay.kz>
2025-11-27 11:31:29 +0500

    MOBILE-1186 - add xcframework

--- Package.swift
+++ Package.swift
@@ -1,4 +1,6 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.1
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
@@ -10,13 +12,12 @@ let package = Package(
         .library(
             name: "FreedomPaymentSdk",
             targets: ["FreedomPaymentSdk"]
-        ),
+        )
     ],
     targets: [
         .binaryTarget(
             name: "FreedomPaymentSdk",
-            url: "https://github.com/freedompay-global/merchant-sdk-ios/releases/download/1.0.1/FreedomPaymentSdk.xcframework.zip",
-            checksum: "457a7dbe7fd574852a1842b3756841b18d71e11c4e6b0047c486c8d1c30947e9"
+            path: "Binary/FreedomPaymentSdk.xcframework"
         )
     ]
 )
